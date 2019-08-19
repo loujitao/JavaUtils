fc7e9ec33b5b04c655845771de9e76ef4bfe3261
loujitao <995268272@qq.com>
2019-08-19 15:33:16 +0800

    修改

--- simpleUtils/src/main/java/com/steve/number/NumberUtil.java
+++ simpleUtils/src/main/java/com/steve/number/NumberUtil.java
@@ -11,11 +11,11 @@ public class NumberUtil {
 
     //取所有整数部分
     private final static String  INT_FORMAT="#";
-    //取一位整数和两位小数
+    //取整数和两位小数
     private final static String  TWO_DECIMAL_FORMAT="0.00";
-    //以百分比方式计数，并取两位小数
-    private final static String  TWO_DECIMAL_BAI_FORMAT="#.##%";
-    //每三位以逗号进行分隔。
+    //以百分比方式计数，并取三位小数     稳定性五九标准：99.999%
+    private final static String  TWO_DECIMAL_BAI_FORMAT="#.###%";
+    //每三位以逗号进行分隔。   美元一般是用这种格式： 本月支出$1,000,000
     private final static String  USA_FORMAT=",###";
     //显示为科学计数法，并取五位小数
     private final static String  SCIENTIFIC_FORMAT="#.#####E0";
@@ -30,6 +30,7 @@ public class NumberUtil {
     public static String getInt(Double data,String pattern){
         return new DecimalFormat(pattern).format(data);
     }
+
 
     public static void main(String[] args) {
         String result=getInt(123.3323423,NumberUtil.TWO_DECIMAL_FORMAT);
